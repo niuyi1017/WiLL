88509658a1f57002ccb334d82caa22a5afe8f988
niuyi1017 <1096196462@qq.com>
2019-02-12 20:38:37 +0800

    mobile adapt

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
+/* eslint-disable */
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import fastClick from 'fastclick'
+
+import './assets/styles/border.css'
+import './assets/styles/reset.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   render: h => h(App),
