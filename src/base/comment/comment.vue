8433ad28b5b043255318c7f863decc791c8d7cff
niuyi1017 <1096196462@qq.com>
2019-03-26 18:34:13 +0800

    comments

---
+++ src/base/comment/comment.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="comment-wrapper">
+    <div class="comment-content" v-for="(item, index) in comments" :key="index"> 
+      <div class="content">
+        <div class="head">
+          <div class="pic">
+            <img :src="item.avatar" alt="" srcset="">
+          </div>
+          <div class="username">
+            <h3>{{item.username}}</h3>
+            <span class="time">{{item.postAt}}</span>  
+          </div>
+          <div class="icon-group">
+            <div class="item">
+              <i class="iconfont icon-reply"></i>
+              <span class="num">{{item.reply_num}}</span>
+            </div>
+            <div class="item">
+              <i class="iconfont icon-like"></i>
+              <span class="num">{{item.like_num}}</span>
+            </div>
+          </div>
+        </div>
+        <div class="body">
+          <p>{{item.content}}</p>
+          <div class="reply" v-for="(reply, innerIndex) in item.replys" :key="innerIndex">
+            <div class="head">
+              <div class="pic">
+                <img :src="reply.from_avatar" alt="" srcset="">
+              </div>
+              <div class="username">
+                {{reply.from_username}} <span class="to"> 回复 </span> {{reply.to_username}}
+              </div>
+            </div>
+            <div class="reply-body">
+              <p>{{reply.content}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="edit-bar">
+      <div class="left item">
+        <i class="iconfont icon-edit-square"></i>
+      </div>
+      <div class="middle">
+        <textarea rows="1" placeholder="发表评论"></textarea>
+      </div>
+      <div class="right item">
+        <i class="iconfont icon-feiji"></i>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Comment',
+  props:{
+    comments: {
+      type: Array,
+      default(){
+        return []
+      }
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import "~@/common/stylus/variable.styl"
+.comment-wrapper
+  &:last-child
+      padding-bottom 1rem
+  .comment-content
+    background #fff
+    margin-top .02rem
+    .content
+      .head
+        height 1rem
+        display flex
+        align-items center
+        .pic
+          height 0.8rem
+          width 0.8rem
+          overflow hidden
+          border-radius 50%
+          box-shadow 0 .1rem .3rem #ccc
+          margin-left .2rem
+          img
+            height 100%
+            width 100%
+        .username
+          flex-grow 1
+          margin 0 .2rem
+          h3
+            line-height .4rem
+          .time
+            color #aaa
+          .to
+            color #000
+        .icon-group
+          width 2.5rem
+          display flex
+          justify-content space-evenly
+          .iconfont
+            margin-right .05rem
+      .body
+        margin-left 1rem
+        line-height .4rem
+        font-size .28rem
+        padding-bottom .2rem
+        p
+          padding-bottom .2rem
+      .reply
+        background $cl-bg2
+        border-radius .15rem
+        margin-right .2rem
+        margin-bottom .1rem
+        .reply-body
+          padding 0 .2rem
+  .edit-bar
+    position fixed
+    bottom 0
+    background $cl-bg2
+    height 1rem
+    width 100%
+    z-index 2
+    display flex
+    .item
+      width 1rem
+      display flex
+      align-items center
+      justify-content center
+      .iconfont 
+        font-size .54rem
+    .left
+      .iconfont
+        color #aaa
+    .right
+      .iconfont
+        color $cl-yellow
+        font-weight 500
+        font-size .68rem
+        text-shadow 0 0 .5rem $cl-yellow
+    .middle
+      flex-grow 1
+      min-height 1rem
+      line-height 1rem
+      textarea 
+        width 100%
+        font-size .32rem
+        background $cl-bg2
+        // border-bottom .02rem
+
+
+</style>
--- src/components/article/article.vue
+++ src/components/article/article.vue
@@ -16,26 +16,62 @@
       </div>
     </div>
     <div class="headline">
-      <h1 class="article-title">在距海最近的大学读书是什么样的体验在距海最近的大学读书是</h1>
+      <h1 class="article-title">在距海最近的大学读书是什么样的体验</h1>
       <div class="meta">
         <div class="time">31 mins ago</div>
         <div class="name">一只小小白</div>
       </div>
     </div>
+    <div class="content">
+
+    </div>
+    <div class="comment-bar">
+      <div class="left">
+        <div class="item read">
+          <span class="icon"><i class="iconfont icon-eye"></i></span>
+          <div class="num">2645</div>
+        </div>
+        <div class="item comment">
+          <span class="icon"><i class="iconfont icon-comment"></i></span>
+          <div class="num">245</div>
+        </div>
+      </div>
+      <div class="item like right">
+        <span class="icon"><i class="iconfont icon-like"></i></span>
+        <div class="num">2.5k</div>
+      </div>
+    </div>
+    <comment :comments="comments"/>
   </div>
 </template>
 <script>
 import MHeader from '@/base/header/header'
+import Comment from '@/base/comment/comment'
+import {getComments} from '@/api/comments'
 export default {
   name: 'Article',
   components: {
-    MHeader
+    MHeader,
+    Comment
   },
   data() {
     return {
       title: '文章正文',
-      showAbs: true
+      showAbs: true,
+      comments: []
     }
+  },
+  methods: {
+    _getComments(){
+      getComments().then((res) => {
+        if(res.code==0&&res.data){
+          this.comments = res.data
+        }
+      })
+    }
+  },
+  mounted() {
+    this._getComments()
   },
 }
 </script>
@@ -50,7 +86,6 @@ export default {
     justify-content space-between
     align-items center
     padding 0 .4rem
-    // box-shadow 0 .2rem .3rem #ccc
     .left
       display flex
       .pic
@@ -81,7 +116,7 @@ export default {
         border-radius 0.25rem
         color $cl-yellow
   .headline
-    height 2rem
+    height 1.8rem
     margin-top .08rem
     background #fff
     padding 0 .4rem
@@ -89,8 +124,8 @@ export default {
     flex-direction column
     justify-content space-evenly
     .article-title
-      line-height 0.6rem
-      font-size .44rem
+      line-height 0.55rem
+      font-size .40rem
     .meta
       display flex
       justify-content flex-start
@@ -100,6 +135,26 @@ export default {
         color #cccccc
       .name
         color $cl-blue
-
-
+  .content
+    height 10rem
+    background #fff
+    margin-bottom 0.1rem
+  .comment-bar
+    height 1rem
+    display flex
+    align-items center
+    justify-content space-between
+    width 100%
+    background #fff
+    .left
+      display flex
+      margin-left .2rem
+    .item
+      display flex
+      margin-right .2rem
+      .iconfont
+        font-size .36rem
+      .num 
+        margin-left .1rem
+        margin-top .05rem
 </style>
