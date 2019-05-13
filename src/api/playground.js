31a2d63d101e9a088fcf202e3fbd2b745a131a95
niuyi1017 <1096196462@qq.com>
2019-05-13 22:56:51 +0800

    ...

--- src/api/playground.js
+++ src/api/playground.js
@@ -13,3 +13,16 @@ export function getMomentList() {
     return Promise.resolve(res.data)
   })
 }
+export function momentLike(moment_id,from,to,recentlyMoment) {
+  const url = '/api/moment/like'
+  const data = {
+    moment_id,
+    from,
+    to,
+    recentlyMoment
+  } 
+  return axios.put(url,data)
+  .then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
